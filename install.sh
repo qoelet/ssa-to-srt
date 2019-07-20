37fc2c3b148af99e69d731a2a831f48507cbad84
Kenny Shen <kenny@machinesung.com>
2019-07-21 02:12:05 +0800

    Update bash scripts with safer flags

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 gcc run.c ssa-to-srt.c -o ssa-to-srt
 install -t $HOME/.local/bin/ ssa-to-srt
 rm ssa-to-srt
--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 gcc test-ssa-to-srt.c ssa-to-srt.c -lrt -lm -o test-ssa-to-srt
 ./test-ssa-to-srt
 rm test-ssa-to-srt
